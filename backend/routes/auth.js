866313266040254235c191ff77d0f8bea81ca66e
bhavanishankarp43-glitch <bhavanishankarp43@gmail.com>
2026-01-31 10:57:17 +0530

    changes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const User = require("../models/User.js")
+const User = require("../Models/User.js")
 const route = express.Router()
 const bcrypt = require("bcrypt")
 
